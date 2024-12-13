f9788a325533432f455d2b2d0437c408639918ff
Dridr1 <a.driano@outlook.com>
2024-12-13 17:54:25 -0300

    feat: ranking services now correctly considers 20 last games

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -6,6 +6,11 @@ import * as api from "../api/api"
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 const DELAY = 1500;
 
+type match = {
+  win: boolean,
+  gameEndTimestamp: number,
+};
+
 @Injectable()
 export class RankingService {
   @Inject(SummonersService)
@@ -18,48 +23,62 @@ export class RankingService {
 
   async create(wins: number, lose: number, winRate: number, gameName: string) {
     try {
-      return await this.prisma.ranking.create({data: {wins: wins, lose: lose, winRate, gameName}})
+      return await this.prisma.ranking.create({ data: { wins: wins, lose: lose, winRate, gameName } })
     } catch (error) {
       console.log(error);
     }
   }
 
+  getWinrate(matches: match[]) {
+    //sorting
+    const sortedMatches = matches.sort((a, b) => b.gameEndTimestamp - a.gameEndTimestamp);
+    const results = sortedMatches.length > 20 ? sortedMatches.slice(0, 20) : sortedMatches;
+    
+    //geting winrate
+    let wins = 0;
+    let loses = 0;
+
+    for (const result of results) {
+      if (result.win) wins++;
+      else loses++;  
+    }
+    const winRate = (wins / (wins + loses)) * 100;
+
+    //return wins, loses and win-rate
+    return {wins, loses, winRate};
+  }
+
+  // TODO: adicionar cronjob dos rankings
   async updateRanking() {
     try {
       await this.remove();
       const summoners = await this.summonersService.findAll();
       for (const summoner of summoners) {
-        let wins = 0;
-        let lose = 0;
-        
         const flex = await this.findMatches(summoner.puuid, 440);
         const soloDuo = await this.findMatches(summoner.puuid, 420);
         const matches = [...soloDuo, ...flex];
-
+        let matchDetailsArray = [];
+        
         for (const match of matches) {
           await delay(DELAY);
           const matchDetails = await api.fechMatchDetails(match, this.createConfig());
           const MatchDetailsByPlayerUuid = matchDetails.data.info.participants.find(({ puuid }) => puuid === summoner.puuid);
-          
-          if(matchDetails.data.info.gameDuration < 300) continue;
-
-          if (MatchDetailsByPlayerUuid.win) {
-            wins++;
-          } else lose++;
+          if (matchDetails.data.info.gameDuration < 300) continue;
+          matchDetailsArray.push({win: MatchDetailsByPlayerUuid.win, gameEndTimestamp: matchDetails.data.info.gameEndTimestamp});
+          console.log(matchDetailsArray);
         }
-        
-        const winRate = (wins / (wins + lose)) * 100;
-        
-        await this.create(wins, lose, winRate, summoner.gameName);
+        const results = this.getWinrate(matchDetailsArray);
+
+        await this.create(results.wins, results.loses, results.winRate, summoner.gameName);
       }
     } catch (error) {
-
+      console.log(error);
     }
   }
 
   async findMatches(puuid: string, queue: number) {
     try {
-      const response = await api.fetchMatchesByPuuid(puuid, queue,this.createConfig());
+      const response = await api.fetchMatchesByPuuid(puuid, queue, this.createConfig());
       return response.data;
     } catch (error) {
 
